9796c15d3186c5c0c3bc669bef98de3c5ecaf8d9
RoccoGibbons <roccogibbons@outlook.com>
2024-07-01 13:14:45 +0100

    Added calculation to  find number of bits needed

--- converter.py
+++ converter.py
@@ -1,5 +1,26 @@
-def main():
-    print("Hello world")
+from math import log2, ceil
 
+def main():
+    convert = int(input("Would you like to convert denary to binary(1) or binary to denary(2)  "))
+    
+    if convert == 1:
+        d_to_b()
+    elif convert == 2:
+        b_to_d()
+    else:
+        print("Input a valid option")
+
+
+def d_to_b():
+    num = int(input("Please input your whole denary number: "))
+    bits = num_of_bits(num)
+    print(bits)
+
+def b_to_d():
+    print()
+
+def num_of_bits(num):
+    bits = ceil(log2(num))
+    return bits
 
 main()
